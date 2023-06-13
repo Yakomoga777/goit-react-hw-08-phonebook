925e3c64ab93f2aa9286fb562c2a58d33ea8d7e7
Roman Petrenko <rpetrenko0105@gmail.com>
2023-06-13 14:37:27 +0300

    редагує стор

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,4 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactSlise } from './slises/contactSlise';
 import { filterSlise } from './slises/filterSlise';
 import {
