610ce08361da1058d30f732c2f1aa7006a1d7c0d
Roman Petrenko <rpetrenko0105@gmail.com>
2023-06-14 15:18:42 +0300

    редагує стилі AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -4,11 +4,14 @@ export const NavigationWrap = styled.div`
   display: flex;
   justify-content: space-between;
 
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
   margin-bottom: 10px;
-
-  border-radius: 10px;
   padding-left: 10px;
   padding-right: 10px;
+  border-radius: 10px;
+
   box-shadow: 0px 0px 15px 0px rgba(24, 106, 117, 1);
 `;
 
