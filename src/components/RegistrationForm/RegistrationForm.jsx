569a638f1e78c77a5371b9a43ee14b842334ba77
Roman Petrenko <rpetrenko0105@gmail.com>
2023-06-14 15:13:54 +0300

    редагує форму реєстрації

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -23,8 +23,6 @@ export const RegistrationForm = () => {
       password,
     };
     dispatch(register(userData));
-    setName('');
-    setEmail('');
     setPassword('');
     console.log(userData);
   };
